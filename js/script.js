9734928bf9b83879ff74c9b1396cda1809d369ab
meht2023 <meht2023@gmail.com>
2022-10-16 15:34:08 +0300

    pravkanenght1

--- js/script.js
+++ js/script.js
@@ -8,10 +8,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из просмотренных фильмов',''),
-      b = prompt('На сколько оценит его?',''),
-      c = prompt('Один из просмотренных фильмов',''),
-      d = prompt('На сколько оценит его?','');
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из просмотренных фильмов',''),
+          b = prompt('На сколько оценит его?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' & b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b; 
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+          
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    console.log('ВЫ класический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('ВЫ киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+      
+
